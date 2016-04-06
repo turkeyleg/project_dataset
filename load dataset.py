2ddfa0a68794fc81dfb85cc22f2a8b5162645556
turkeyleg <afjylkka@gmail.cmo>
2016-04-06 16:49:40 -0400

    more delinq stuff

--- load dataset.py
+++ load dataset.py
@@ -146,12 +146,25 @@ print 'done merging'
 #print merged.zero_balance_eff_dt.unique()
 
 is_delinq = lambda x: (x not in ('0', 'XX', 'R'))
+# apply boolean indicating whether the loan status is delinquent
 merged['is_delinquent'] = merged['current_loan_delinq_status'].apply(is_delinq)
 
 gb = merged.groupby(level=0)
 # should return a series, for each loan give the index of the first thing that's delinquent
-get_first_delinq = lambda x: x.idxmax() if x.any() else None
-gb.is_delinquent.apply(get_first_delinq)
+get_first_delinq = lambda x: x.idxmax()[1] if x.any() else pd.NaT
+first_delinq = gb.is_delinquent.apply(get_first_delinq)
+first_delinq.name = 'first_delinquency'
+merged = pd.merge(merged, pd.DataFrame(first_delinq), left_index=True, right_index=True)
+# get the monthly reporting date from the datetime index
+i=merged.index.to_series().apply(lambda x: x[1])
+past_delinq = (~ pd.isnull(merged.first_delinquency)) & (i > merged.first_delinquency )
+print merged.shape
+print past_delinq.sum()
+past_delinq = merged[past_delinq]
+past_delinq_idx = past_delinq.index
+merged.drop(past_delinq_idx, inplace=True)
+print merged.shape
+print 'ready'
 #gb['is_delinquent'].idxmax() if gb['is_delinquent'].any() else None
 
 
